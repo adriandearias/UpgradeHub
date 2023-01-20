833c7a30c269eddbbbaa4aed3e920e6249bd9668
adriandearias <deariasadrian@gmail.com>
2023-01-20 20:33:44 +0100

    added S1 extra-6 js

--- javascript/ejerciciosJs/S1/extra/exercise-6.js
+++ javascript/ejerciciosJs/S1/extra/exercise-6.js
@@ -1,6 +1,12 @@
-//Usa un bucle para recorrer el array de peliculas (`movies`) y añade al array `goodProducts` los que tengan más de 20 ventas (`sellCount`) y al array `badProducts` los que tengan menos.
+//Usa un bucle para recorrer el array de productos (`products`) y añade al array `goodProducts` los que tengan más de 20 ventas (`sellCount`) y al array `badProducts` los que tengan menos.
 
 const goodProducts = [];
 const badProducts = [];
 const products = [{name: 'Gorra de rodilla', sellCount: 10},{name: 'Pantalón de pana', sellCount: 302},{name: 'Reloj de papel albal', sellCount: 23},{name: 'Inpar de zapatos', sellCount: 6}];
 
+for (const product of products) {
+    product.sellCount > 20 ? goodProducts.push(product) : badProducts.push(product)
+}
+
+console.log('Los productos con más ventas: ',goodProducts);
+console.log('Los productos con menos(peores) ventas: ',badProducts);
