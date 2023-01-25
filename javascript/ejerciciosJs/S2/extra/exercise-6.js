e0ae2ac471b8e55298b3c428e7927789733c6ff2
adriandearias <deariasadrian@gmail.com>
2023-01-25 14:19:28 +0100

    added javascript/ejerciciosJs/S2/extra/exercise-6.js unfinished

---
+++ javascript/ejerciciosJs/S2/extra/exercise-6.js
@@ -0,0 +1,14 @@
+// Crea una función llamada ``swap`` que reciba un array y dos parametros que sean indices del array. La función deberá 
+// intercambiar la posición de los valores de los indices que hayamos enviado como parametro. Retorna el array 
+// resultante.
+
+// Sugerencia de array:
+// ['Mesirve', 'Cristiano Romualdo', 'Fernando Muralla', 'Ronalguiño']
+
+
+
+function swap(array, index){
+
+}
+
+const arrayNames = ['Mesirve', 'Cristiano Romualdo', 'Fernando Muralla', 'Ronalguiño']
