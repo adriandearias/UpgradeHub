49f3ea733be5a8956b0a260d0ba84f74b23618db
Adrian Cano <deariasadrian@gmail.com>
2023-01-27 23:10:32 +0100

    added exercise of js javascript/ejerciciosJs/extra/exercise-3.js

--- javascript/ejerciciosJs/extra/exercise-3.js
+++ javascript/ejerciciosJs/extra/exercise-3.js
@@ -1,5 +1,14 @@
-En base al siguiente array cuenta cuantas letras "a" tenemos. El resultado debería ser 9.
+//En base al siguiente array cuenta cuantas letras "a" tenemos. El resultado debería ser 9.
 
-```js
 const animals = ["Salamandra montesa", "Delinicio", "Tigre de puntos", "Saltamontañas"]
-```
+let contador = 0;
+
+for (const animal of animals) {
+    for (let i = 0; i < animal.length; i++) {
+        if (animal[i] === 'a' || animal[i] === 'A') {
+            contador++
+        }
+    }
+}
+
+console.log(`En el array -> [${animals}] hay ${contador} letras "a"`);
