1ca1cf343cba478f0a0c15a1bfb75e0e15301b73
adriandearias <deariasadrian@gmail.com>
2023-01-28 18:05:26 +0100

    x

--- javascript/ejerciciosJs/extra/exercise-4.js
+++ javascript/ejerciciosJs/extra/exercise-4.js
@@ -1,10 +1,10 @@
-Segun la siguiente lista, ordena los elementos en base a la propiedad .order.
+//Segun la siguiente lista, ordena los elementos en base a la propiedad .order.
 
-```js
 const list = [
     {name: "La tulipán", gender: "pop", order: 3},
     {name: "Calle falsa 123", gender: "rap", order: 2},
     {name: "Z balvin", gender: "reggeaton", order: 4},
     {name: "Neng de castefa", gender: "pop", order: 1}
 ]
-```
+
+
