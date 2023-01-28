64c2a75add9e2531a489f859524ac374f67b32ff
Adrian Cano <deariasadrian@gmail.com>
2023-01-28 21:18:35 +0100

    added JS javascript/ejerciciosJs/extra/exercise-7.js

--- javascript/ejerciciosJs/extra/exercise-7.js
+++ javascript/ejerciciosJs/extra/exercise-7.js
@@ -1,9 +1,16 @@
-Dado el siguiente string
+//Dado el siguiente string
 
-```js
 const text = `gracioso-pero-no-gracioso-de-risa-gracioso-de-raro`
-```
 
-transformalo en el siguiente (Mayusculas incluidas)
+//transformalo en el siguiente (Mayusculas incluidas)
+//`Gracioso Pero No Gracioso De Risa Gracioso De Raro`
 
-`Gracioso Pero No Gracioso De Risa Gracioso De Raro`
+let textNew = text.split('-')
+
+for (let i = 0; i < textNew.length; i++) {
+    textNew[i] = textNew[i].charAt(0).toUpperCase() + textNew[i].slice(1)
+}
+
+const strNew = textNew.join(" ")
+
+console.log(strNew);
