1a3d93046305e4bafa20d529aa952f194518b0c6
Adrian Cano <deariasadrian@gmail.com>
2023-01-28 20:43:35 +0100

    added JS javascript/ejerciciosJs/extra/exercise-5.js

--- javascript/ejerciciosJs/extra/exercise-5.js
+++ javascript/ejerciciosJs/extra/exercise-5.js
@@ -1,16 +1,14 @@
-Al siguiente javascript no funciona como deberia...podrias arreglarlo?
+// Al siguiente javascript no funciona como deberia...podrias arreglarlo?
+// Debería mostrar por consola "Juan Pui".
 
-Debería mostrar por consola "Juan Pui".
 
-```js
 function a(){
     const name = "Juan Pui";
     b(name);
 }
 
-function b(){
+function b(name){
     console.log(name)
 }
 
 a();
-```
