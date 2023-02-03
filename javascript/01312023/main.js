d0cf64bd5c84f776c2128a33fbd9996713a38b4b
adriandearias <deariasadrian@gmail.com>
2023-02-03 21:25:59 +0100

    added exercise of JS ToDoList

--- javascript/01312023/main.js
+++ javascript/01312023/main.js
@@ -5,7 +5,9 @@
 const db = {
   tareas: [],
   id: 0,
+  tareasEliminadas: []
 };
+
 
 const principal = document.querySelector("#lista-de-tareas");
 const input = document.createElement("input");
@@ -40,14 +42,16 @@ function addTarea() {
   const textoTarea = input.value;
   //trim se puede hacer sobre los strings para quitar todos los espacios
   if (textoTarea.trim() !== "" && !db.tareas.map(x=>x.texto).includes(input.value)) {
-    db.id++;
+    db.id++
     db.tareas.push({ id: db.id, texto: textoTarea, hecho: false });
     pintarTareas();
+  }else{
+    alert('La tarea ya existe en la Lista de Tareas!*')
   }
   input.value = "";
   input.focus();
 
-  //console.log(db.tareas);
+  console.log(db);
 }
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -66,6 +70,7 @@ input.addEventListener("keyup", (event) => {
     addTarea();
   }
 });
+
 
 //crea una funcion pintarTareas() que recorra las tareas de
 // la base de datos y las pinte en el ul, utilizando elementos li
@@ -98,6 +103,12 @@ function pintarTareas() {
     //añadimos atributos a los botones para poder acceder a ellos y darle funcionalidad
     btnD.setAttribute("class", `btn-d-${tarea.id}`)
     btnX.setAttribute("class", `btn-x-${tarea.id}`)
+    li.setAttribute("class", `li-tarea-${tarea.id}`)
+    span.setAttribute("class", `span-global span-${tarea.id}`)
+    if(tarea.hecho === true){ 
+      span.style.color = 'green'
+      span.style.textDecoration = 'line-through'
+    }
     btnD.innerHTML = "✔️";
     btnX.innerHTML = "🗑️";
 
@@ -108,8 +119,9 @@ function pintarTareas() {
     div.appendChild(span);
     li.appendChild(div);
     ul.appendChild(li);
-
+    
   }
+  btnCheck()
 }
 
 // En la funcion addTarea, cambiala de manera que ya no se puedan
@@ -121,14 +133,46 @@ function pintarTareas() {
 // {id_number, texto: string, hecha: boolean} - Hecho
 
 //Darle funcionalidad a los botones para que borre de la lista de tareas al darle a la papelara "btnX"
-//cambiar estilo en el botonD que lo cambie de hecha:true y poner texto en verde
+//cambiar estilo en el botonD que lo cambie de hecha:true y poner texto en verde/tachada
 
+const btnCheck = () => {
+  for (const tarea of db.tareas) {
+    const btnX = document.querySelector(`.btn-x-${tarea.id}`)
+    const btnD = document.querySelector(`.btn-d-${tarea.id}`)
+    btnX.addEventListener('click', () =>{
+      borrarTarea(tarea.id)
+    })
+    btnD.addEventListener('click', () =>{
+      tareaCompletada(tarea.id)
+    })
+  }
+}
 
-// btnD.addEventListener('click', ()=>{
-    //principal.removeChild(div)
+const borrarTarea = (tareaId) => {
+  const ul = document.querySelector('ul')
+  const borrarLiTarea = document.querySelector(`.li-tarea-${tareaId}`)
+  ul.removeChild(borrarLiTarea)
+  db.tareasEliminadas.push(db.tareas.splice(tareaId-1, 1))
+  console.log(db.tareasEliminadas);
+}
 
-    //div.parentNode.removeChild(div)
-    //const eliminado = (div.parentNode).removeChild(div)
-    //principal.appendChild(eliminado)
-// })
+const tareaCompletada = (tareaId) => {
+  const spanCompletado = document.querySelector(`.span-${tareaId}`)
+  spanCompletado.style.color = 'green'
+  spanCompletado.style.textDecoration = 'line-through'
+  db.tareas[tareaId-1].hecho = true
+}
 
+// const pintarTareasBorradas =() =>{
+//   for (const tarea of db.tareasEliminadas) {
+//     const li = document.createElement("li");
+//     const span = document.createElement("span");
+//     const div = document.createElement("div");
+
+//     div.appendChild(div);
+//     li.appendChild(span);
+// }
+
+// // const pintarTareasCompletadas =() =>{
+
+// // }
