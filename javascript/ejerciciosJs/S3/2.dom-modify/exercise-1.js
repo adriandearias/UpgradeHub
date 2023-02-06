84300b49eafe6dbeb69b80f008ea0d2e810c81c4
Adrian Cano <deariasadrian@gmail.com>
2023-02-06 13:58:06 +0100

    added exercises of js

---
+++ javascript/ejerciciosJs/S3/2.dom-modify/exercise-1.js
@@ -0,0 +1,6 @@
+//Inserta dinamicamente en un html un div vacio con javascript.
+
+const body$$ = document.querySelector('body') 
+
+const div$$ = document.createElement('div')
+body$$.appendChild(div$$)
