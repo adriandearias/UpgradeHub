b12b49598eee467b30863e51887b80664b491edd
Adrian Cano <deariasadrian@gmail.com>
2023-02-11 00:08:35 +0100

    added exercises S7/4map/ 2&3 of js

--- javascript/ejerciciosJs/S7/4.map/exercise-2.js
+++ javascript/ejerciciosJs/S7/4.map/exercise-2.js
@@ -1,11 +1,17 @@
 // Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad
 //  .name y cambia el nombre a 'Anacleto' en caso de que empiece por 'A'.
 
-
 const users = [{id: 1, name: 'Abel'},
     {id:2, name: 'Julia'},
     {id:3, name: 'Pedro'},
     {id:4, name: 'Amanda'}
 ];
 
+const newUsers = users.map(user => {
+    if(user.name.charAt(0)==='A'){
+        user.name = 'Anacleto'
+    }
+    return user.name
+})
 
+console.log(newUsers);
--- javascript/ejerciciosJs/S7/4.map/exercise-3.js
+++ javascript/ejerciciosJs/S7/4.map/exercise-3.js
@@ -8,4 +8,11 @@ const cities = [{isVisited:true, name: 'Tokyo'},
     {isVisited:false, name: 'Seul'}
 ];
 
+const citiesNew = cities.map(city => {
+    if(city.isVisited === true){
+        city.name += ' Visitado'
+    }
+    return city.name
+})
 
+console.log(citiesNew);
