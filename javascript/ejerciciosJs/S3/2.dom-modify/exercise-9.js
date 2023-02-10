2cb0b7930a58c85b0b71c0213e95d3d9a8f5d55f
adriandearias <deariasadrian@gmail.com>
2023-02-10 17:41:59 +0100

    added javascript/ejerciciosJs/S3/2.dom-modify/exercise-9.js

--- javascript/ejerciciosJs/S3/2.dom-modify/exercise-9.js
+++ javascript/ejerciciosJs/S3/2.dom-modify/exercise-9.js
@@ -1,2 +1,12 @@
 // Basandote en el siguiente html y javascript,inserta p con el texto 'Voy dentro!',
 // dentro de todos los div con la clase .fn-insert-here
+
+const fnInsertHere = document.querySelectorAll('.fn-insert-here')
+
+console.log(fnInsertHere);
+
+for (const element of fnInsertHere) {
+    const p$$ = document.createElement('p')
+    element.appendChild(p$$)
+    p$$.innerHTML = 'Voy dentro!'
+}
