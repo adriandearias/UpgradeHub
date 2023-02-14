c45b4dc24a6f13d1da8709c6d3e457a2ae5f73ec
adriandearias <deariasadrian@gmail.com>
2023-02-14 19:43:58 +0100

    changed  switch case for object in setColors()

--- pokeapi/api.js
+++ pokeapi/api.js
@@ -1,5 +1,6 @@
 //get 151 Pokemons(first generation)
-export async function getPokemons(arrayPokemons){
+export async function getPokemons(){
+    const arrayPokemons = []
     for (let i = 1; i < 152; i++) {
         const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
         const pokemonResultToObject = await result.json()   
--- pokeapi/main.js
+++ pokeapi/main.js
@@ -1,228 +1,166 @@
 //@import modules
-import {getPokemons} from "./api.js";
+import { getPokemons } from "./api.js";
 
 //Global variable declaration
-const arrayPokemons = [];
+let arrayPokemons = [];
 
 //function map
-function mapPokemons(arrayPokemons){
+function mapPokemons(arrayPokemons) {
   const mappedPokemons = arrayPokemons.map((pokemon) => ({
     id: pokemon.id,
     name: pokemon.name,
-    type: pokemon.types.map(type => type.type.name),
+    type: pokemon.types.map((type) => type.type.name),
     weight: pokemon.weight,
     img: pokemon.sprites.front_shiny,
-    abilities: pokemon.abilities[0].ability.name
-  }))
-  return mappedPokemons
+    abilities: pokemon.abilities[0].ability.name,
+  }));
+  return mappedPokemons;
 }
 
 //insert html to show/draw Pokemons
-function drawPokemons(mappedPokemons) {  
-  const divContent$$ = document.querySelector('.div-content')
-  divContent$$.innerHTML = ''
-    for (const pokemon of mappedPokemons) {
-      const divPokemon$$ = document.createElement('div')
-      const img$$ = document.createElement("img");
-      const span$$ = document.createElement("span");
-      const ulDescription$$ = document.createElement("ul")
-      const li$$ = document.createElement("li");
-      const li2$$ = document.createElement("li");
-      const li3$$ = document.createElement("li");
+function drawPokemons(mappedPokemons) {
+  const divContent$$ = document.querySelector(".div-content");
+  divContent$$.innerHTML = "";
+  for (const pokemon of mappedPokemons) {
+    const divPokemon$$ = document.createElement("div");
+    const img$$ = document.createElement("img");
+    const span$$ = document.createElement("span");
+    const ulDescription$$ = document.createElement("ul");
+    const li$$ = document.createElement("li");
+    const li2$$ = document.createElement("li");
+    const li3$$ = document.createElement("li");
 
-      divContent$$.appendChild(divPokemon$$)
-      divPokemon$$.appendChild(img$$)
+    divContent$$.appendChild(divPokemon$$);
+    divPokemon$$.appendChild(img$$);
 
-      ulDescription$$.setAttribute('class', 'ul-description')
-      divPokemon$$.setAttribute('class', 'div-pokemon')
-      Object.assign(divPokemon$$, {
-        className: `div-pokemon color-${pokemon.type[0]}`
-      })
-      setColor(divPokemon$$, pokemon.type[0])
-      img$$.setAttribute('src', pokemon.img)
-      img$$.setAttribute('class', 'img-pokemon')
+    ulDescription$$.setAttribute("class", "ul-description");
+    divPokemon$$.setAttribute("class", "div-pokemon");
+    Object.assign(divPokemon$$, {
+      className: `div-pokemon color-${pokemon.type[0]}`,
+    });
+    // divPokemon$$.classList.add(`div-pokemon color-${pokemon.type[0]}`)
+    //divPokemon$$.classList -> cambiar
+    setColor(divPokemon$$, pokemon.type[0]);
+    img$$.setAttribute("src", pokemon.img);
+    img$$.setAttribute("class", "img-pokemon");
 
-      divPokemon$$.appendChild(span$$)
-      const pokemonUppercase = pokemon.name
-      span$$.innerHTML = `${pokemon.id}.${pokemonUppercase.charAt(0).toUpperCase() + pokemon.name.slice(1)}`
+    divPokemon$$.appendChild(span$$);
+    const pokemonUppercase = pokemon.name;
+    span$$.innerHTML = `${pokemon.id}.${
+      pokemonUppercase.charAt(0).toUpperCase() + pokemon.name.slice(1)
+    }`;
 
-      divPokemon$$.appendChild(ulDescription$$)
-      ulDescription$$.appendChild(li$$)
-      ulDescription$$.appendChild(li2$$)
-      ulDescription$$.appendChild(li3$$)
-      li$$.innerHTML = 'Type ⫸ '
-      pokemon.type.map( type =>li$$.innerHTML += `${type.toUpperCase()} ` )
-      li2$$.innerHTML = `Weight ⫸ ${pokemon.weight} kg`
-      const abilityUppercase = pokemon.abilities
-      li3$$.innerHTML = `Ability ⫸ ${abilityUppercase.charAt(0).toUpperCase() + pokemon.abilities.slice(1)}`
-    }
-}
-//function set color, used to color buttons and div-pokemon
-function setColor(element, type){
-  switch (type) {
-    case 'grass':
-      Object.assign(element.style, {
-        background: '#63bb5b'
-      })
-      break;
-    case 'poison':
-      Object.assign(element.style, {
-        background: '#ab6ac8'
-      })
-      break;
-    case 'fire':
-      Object.assign(element.style, {
-        background: '#ff9c54'
-      })
-      break;
-    case 'flying':
-      Object.assign(element.style, {
-        background: '#8fa8dd'
-      })
-      break;
-    case 'water':
-      Object.assign(element.style, {
-        background: '#4d90d5'
-      })
-      break;
-    case 'bug':
-      Object.assign(element.style, {
-        background: '#90c12c'
-      })
-      break;
-    case 'normal':
-      Object.assign(element.style, {
-        background: '#9099a1'
-      })
-      break;
-    case 'electric':
-      Object.assign(element.style, {
-        background: '#ffa710'
-      })
-      break;
-    case 'ground':
-      Object.assign(element.style, {
-        background: '#d97746'
-      })
-      break;
-    case 'fairy':
-      Object.assign(element.style, {
-        background: '#ec8fe6'
-      })
-      break;
-    case 'fighting':
-      Object.assign(element.style, {
-        background: '#ce4069'
-      })
-      break;
-    case 'psychic':
-      Object.assign(element.style, {
-        background: '#f97176'
-      })
-      break;
-    case 'rock':
-      Object.assign(element.style, {
-        background: '#c7b78b'
-      })
-      break;
-    case 'steel':
-      Object.assign(element.style, {
-        background: '#5a8ea1'
-      })
-      break;
-    case 'ice':
-      Object.assign(element.style, {
-        background: '#74cec0'
-      })
-      break;
-    case 'ghost':
-      Object.assign(element.style, {
-        background: '#5269ac'
-      })
-      break;
-    case 'dragon':
-      Object.assign(element.style, {
-        background: '#0a6dc4'
-      })
-      break;
+    divPokemon$$.appendChild(ulDescription$$);
+    ulDescription$$.appendChild(li$$);
+    ulDescription$$.appendChild(li2$$);
+    ulDescription$$.appendChild(li3$$);
+    li$$.innerHTML = "Type ⫸ ";
+    pokemon.type.map((type) => (li$$.innerHTML += `${type.toUpperCase()} `));
+    li2$$.innerHTML = `Weight ⫸ ${pokemon.weight} kg`;
+    const abilityUppercase = pokemon.abilities;
+    li3$$.innerHTML = `Ability ⫸ ${
+      abilityUppercase.charAt(0).toUpperCase() + pokemon.abilities.slice(1)
+    }`;
   }
 }
-//input event 
-const setInput = (pokemons) =>{
-  const input$$ = document.getElementById('find-input');
-  input$$.addEventListener('keyup', () =>{
-    filterPokemons(pokemons, input$$.value)
-  })
+//function set color, used to color buttons and div-pokemon
+function setColor(element, type) {
+  const colors = {
+    grass: "#63bb5b",
+    poison: "#ab6ac8",
+    fire: "#ff9c54",
+    flying: "#8fa8dd",
+    water: "#4d90d5",
+    bug: "#90c12c",
+    normal: "#9099a1",
+    electric: "#ffa710",
+    ground: "#d97746",
+    fairy: "#ec8fe6",
+    fighting: "#ce4069",
+    psychic: "#f97176",
+    rock: "#c7b78b",
+    steel: "#5a8ea1",
+    ice: "#74cec0",
+    ghost: "#5269ac",
+    dragon: "#0a6dc4",
+  };
+  Object.assign(element.style, {
+    background: colors[type],
+  });
 }
+//input event
+const setInput = (pokemons) => {
+  const input$$ = document.getElementById("find-input");
+  input$$.addEventListener("keyup", () => {
+    filterPokemons(pokemons, input$$.value);
+  });
+};
 
 //filter pokemons by input event value
-const filterPokemons = (pokemons, inputValue) =>{
-  const filteredPokemons = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(inputValue.toLowerCase()))
-  return drawPokemons(filteredPokemons)
-}
+const filterPokemons = (pokemons, inputValue) => {
+  const filteredPokemons = pokemons.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(inputValue.toLowerCase())
+  );
+  drawPokemons(filteredPokemons);
+};
 
 //draw and color buttons function
-function drawButtons(pokemons){
-  const types = []
-  const btnAll$$ = document.createElement('button')
-  const divBtn$$ = document.querySelector('.div-btn')
+function drawButtons(pokemons) {
+  const types = [];
+  const btnAll$$ = document.createElement("button");
+  const divBtn$$ = document.querySelector(".div-btn");
 
-  divBtn$$.appendChild(btnAll$$)
-  btnAll$$.innerHTML = 'Show All'
+  divBtn$$.appendChild(btnAll$$);
+  btnAll$$.innerHTML = "Show All";
   Object.assign(btnAll$$, {
     className: `btn-type`,
-    id: 'btn-all'
-  })
+    id: "btn-all",
+  });
   // setListener to btn to show all
-  setBtnAll(btnAll$$, pokemons)
+  setBtnAll(btnAll$$, pokemons);
   //iterate to get types
-  for (const pokemon of pokemons){
+  for (const pokemon of pokemons) {
     for (const type of pokemon.type) {
-      if(!types.includes(type)){
-        types.push(type)   
+      if (!types.includes(type)) {
+        types.push(type);
       }
     }
   }
   for (const type of types) {
-      const btn$$ = document.createElement('button')
-      divBtn$$.appendChild(btn$$)
-      //first letter of type to mayus
-      let typeUppercase = type
-      btn$$.innerHTML = typeUppercase.charAt(0).toUpperCase() + type.slice(1) 
-      Object.assign(btn$$, {
-        className: `btn-type btn-${type}`
-      })
-      //color button bg
-      setColor(btn$$, type)
+    const btn$$ = document.createElement("button");
+    divBtn$$.appendChild(btn$$);
+    //first letter of type to mayus
+    let typeUppercase = type;
+    btn$$.innerHTML = typeUppercase.charAt(0).toUpperCase() + type.slice(1);
+    Object.assign(btn$$, {
+      className: `btn-type btn-${type}`,
+    });
+    //color button bg
+    setColor(btn$$, type);
   }
-  return setButtons(types, pokemons)
+  setButtons(types, pokemons);
 }
 
-const setButtons = (types, pokemons) =>{
+const setButtons = (types, pokemons) => {
   for (const type of types) {
-    const btn = document.querySelector(`.btn-${type}`)
-    btn.addEventListener('click', () =>{
-      drawPokemons(pokemons.filter(pokemon => pokemon.type.includes(type)))
-    })
+    const btn = document.querySelector(`.btn-${type}`);
+    btn.addEventListener("click", () => {
+      drawPokemons(pokemons.filter((pokemon) => pokemon.type.includes(type)));
+    });
   }
-}
+};
 
-function setBtnAll(btnAll, pokemons){
-  btnAll.addEventListener('click', () =>{
-    drawPokemons(pokemons)
-  })
+function setBtnAll(btnAll, pokemons) {
+  btnAll.addEventListener("click", () => {
+    drawPokemons(pokemons);
+  });
 }
+window.addEventListener("DOMContentLoaded", async () => {
+  console.log("DOM fully loaded and parsed");
+  arrayPokemons = await getPokemons();
 
-//init
-const init = async () => {
-    await getPokemons(arrayPokemons);
-    window.addEventListener('DOMContentLoaded', () => {});
-    console.log('DOM fully loaded and parsed');
-    const mappedPokemons = mapPokemons(arrayPokemons) //pokemons
-    setInput(mappedPokemons)
-    drawButtons(mappedPokemons)
-    drawPokemons(mappedPokemons)
-  };
-  
-  init();
-  
+  const mappedPokemons = mapPokemons(arrayPokemons); //pokemons
+  setInput(mappedPokemons);
+  drawButtons(mappedPokemons);
+  drawPokemons(mappedPokemons);
+});
