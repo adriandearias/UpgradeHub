bdfe7c186c38517e5121d346cb4295942657627a
adriandearias <deariasadrian@gmail.com>
2023-02-21 21:57:17 +0100

    added first class of node 2212023

--- node/02212023/index.js
+++ node/02212023/index.js
@@ -1,7 +1,31 @@
 const express = require("express");
-const PORT = 8000;
+//traemos la libreria dotenv y la config
+require("dotenv").config();  //PORT es la variable de entorno de .env
+const PORT = process.env.PORT;
 //ejecutar libreria express
 const server = express();
+
+server.use("/movies", (req, res) =>{
+    const movies = [
+        {
+            tittle: "Titanic",
+            cover: "https://url",
+            duration: 245
+        },
+        {
+            tittle: "Avatar",
+            cover: "https://url",
+            duration: 300
+        }
+    ]
+    //res.send("Hola desde barra");
+    res.json(movies);
+});
+
+server.use("/", (req, res) =>{
+    res.send("Accede a /movies para ver peliculas");
+});
+
 //el servidor tiene varias funciones, se puede usar(server.use()), se puede poner a escuchar (server.listen());
 server.listen(PORT, () =>{
     console.log("Server is running -> http://localhost"+PORT);
