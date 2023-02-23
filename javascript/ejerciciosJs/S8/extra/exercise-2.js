6ae90a358c576ee4632d0f76341d8faf7b6c912d
adriandearias <deariasadrian@gmail.com>
2023-02-23 18:35:09 +0100

    added js S8 extra e2

--- javascript/ejerciciosJs/S8/extra/exercise-2.js
+++ javascript/ejerciciosJs/S8/extra/exercise-2.js
@@ -1,16 +1,45 @@
 fetch('http://localhost:3000/diary')
     .then(response => response.json())
     .then(json => {
-        console.log(json)
         const body = document.querySelector('body');
         const container = document.createElement('div');;
         let dates = []
         body.appendChild(container).setAttribute('class', 'container')
-        
-        for (const movie of json) {
-            console.log(movie);
-            dates.push(movie.date.sort((a, b) => a.date - b.date));
-        }
 
-        console.log(dates);
+        json.sort((a,b) => {
+            var dateA = new Date(a.date);
+            var dateB = new Date(b.date);
+            return dateA - dateB
+        })
+
+        const drawItems = ((json) => {
+            for (const movie of json) {
+                const divNote = document.createElement('div')
+                const headerThree = document.createElement('h3')
+                const headerFive = document.createElement('h5')
+                const p = document.createElement('p');
+                const button = document.createElement('button')
+    
+                container.appendChild(divNote).setAttribute('class', 'note')
+                divNote.appendChild(headerThree)
+                divNote.appendChild(headerFive)
+                divNote.appendChild(p)
+                divNote.appendChild(button).setAttribute('data-key', movie.id)
+                button.innerHTML = 'Borrar'
+    
+                headerThree.innerHTML = movie.title
+                headerFive.innerHTML = movie.date
+                p.innerHTML = movie.description
+            }
+        })
+
+        drawItems(json);
+        const allBtns = document.querySelectorAll('[data-key]');
+        console.log(allBtns);
+
+        for (const btn of allBtns) {
+            btn.addEventListener('click', () =>{
+                btn.parentElement.remove();
+        })
+        }
     })
