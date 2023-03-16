fe78a5c758cdd2e12b27e50e0fc0a6bc2ec0cd36
adriandearias <deariasadrian@gmail.com>
2023-03-16 23:43:30 +0100

    added unfinished exercise of angular-avengers

---
+++ angular/avengers-angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListComponent } from './list/list.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ListComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular/avengers-angular/src/app/core/avengers.interface.ts
@@ -0,0 +1,7 @@
+export interface AvengersInterface{
+    hero: string;
+    name:string;
+    link: string;
+    img: string;
+    isActive: boolean;
+}
