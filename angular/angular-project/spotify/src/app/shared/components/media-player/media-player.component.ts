8d87139031c695f67263eceaaf462b93e04aac28
adriandearias <deariasadrian@gmail.com>
2023-04-07 22:37:27 +0200

    .

--- angular/angular-project/spotify/src/app/shared/components/media-player/media-player.component.ts
+++ angular/angular-project/spotify/src/app/shared/components/media-player/media-player.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
   styleUrls: ['./media-player.component.scss']
 })
-export class MediaPlayerComponent {
+export class MediaPlayerComponent implements OnInit{
+  mockCover: any = {
+    cover: 'yest',
+    name:'test',
+    album: 'test'
+  }
 
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
