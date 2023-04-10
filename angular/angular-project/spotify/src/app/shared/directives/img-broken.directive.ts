962bd632f9fe6900d853dbda7da2045c8e19511e
adriandearias <deariasadrian@gmail.com>
2023-04-10 23:36:28 +0200

    added directives

--- angular/angular-project/spotify/src/app/shared/directives/img-broken.directive.ts
+++ angular/angular-project/spotify/src/app/shared/directives/img-broken.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appImgBroken]'
+  selector: 'img[appImgBroken]'
 })
 export class ImgBrokenDirective {
+  @Input() customImg: string = ''
+  @HostListener('error') handleError(): void{
+    const eleNative = this.theHost.nativeElement
+    console.log('ERROR: img -> broken:' ,this.theHost);
+    // eleNative.src = '../../../assets/images/no-img.jpg'
+    eleNative.src = this.customImg
+  } 
 
-  constructor() { }
+  constructor(private theHost: ElementRef) { 
+    
+  }
 
 }
