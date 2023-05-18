b1924e0ceacc8c61fefe042174c6ac3445e754a9
adriandearias <deariasadrian@gmail.com>
2023-05-18 21:59:09 +0200

    php

---
+++ php/help.php
@@ -0,0 +1,12 @@
+symfony new MyFirstSimfonyProject
+symfony server:start -d
+
+
+composer require symfony/maker-bundle --dev
+symfony console make:controller DefaultController
+
+
+composer require twig
+composer require doctrine
+
+
