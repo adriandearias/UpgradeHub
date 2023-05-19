1839cbe28093443ed0dbd4ca0b6d57a031853c89
adriandearias <deariasadrian@gmail.com>
2023-05-19 23:57:29 +0200

    added config mysql ddbb

--- php/help.php
+++ php/help.php
@@ -10,3 +10,9 @@ composer require twig
 composer require doctrine
 
 
+CREATE BBDD doctrine
+
+Apache config -> $cfg['Servers'][$i]['host'] = '127.0.0.1:3360';
+DATABASE_URL="mysql://root:@127.0.0.1:3360/pokemonbbdd"
+php bin/console doctrine:database:create
+
