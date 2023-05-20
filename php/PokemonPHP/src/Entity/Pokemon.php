4599b37ed5fd86e1ab48ea48356d853afec10a4b
adriandearias <deariasadrian@gmail.com>
2023-05-20 18:18:38 +0200

    5202023

---
+++ php/PokemonPHP/src/Entity/Pokemon.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PokemonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PokemonRepository::class)]
+class Pokemon
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $code = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?int
+    {
+        return $this->code;
+    }
+
+    public function setCode(?int $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+}
--- php/help.php
+++ php/help.php
@@ -1,6 +1,7 @@
 symfony new MyFirstSimfonyProject
 symfony server:start -d
 
+composer i 
 
 composer require symfony/maker-bundle --dev
 symfony console make:controller DefaultController
@@ -14,5 +15,14 @@ CREATE BBDD doctrine
 
 Apache config -> $cfg['Servers'][$i]['host'] = '127.0.0.1:3360';
 DATABASE_URL="mysql://root:@127.0.0.1:3360/pokemonbbdd"
-php bin/console doctrine:database:create
 
+php bin/console doctrine:database:create
+php bin/console doctrine:database:migrations:migrate
+php bin/console doctrine:fixtures:load
+
+composer require symfony/maker-bundle --dev
+php bin/console make:entity
+php bin/console make:migration
+
+
+composer require symfony/http-client
